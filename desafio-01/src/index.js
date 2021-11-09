5c992f0bba291ba5e98b2bc2ad3a919e933abdc2
Matheus Martins Sarmento <matheusms280100@gmail.com>
2021-11-08 21:12:02 -0300

    add middleware and list all todos

--- desafio-01/src/index.js
+++ desafio-01/src/index.js
@@ -11,7 +11,17 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.header;
+  const user = users.find((user) => user.username === username);
+
+  if (!user)
+    return response.status(404).json({
+      error: "This account do not exists",
+    });
+
+  request.user = user;
+
+  return next();
 }
 
 app.post("/users", (request, response) => {
@@ -32,7 +42,8 @@ app.post("/users", (request, response) => {
 });
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  return response.json(user.todos);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
